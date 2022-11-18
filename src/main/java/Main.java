1e210215fe5468e589279f8e56d1343bdd1dfe99
gebreGL <105040707+gebreGL@users.noreply.github.com>
2022-11-18 11:31:43 +0100

    18.11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
 import entities.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Main {
 
     public static void main(String[] args) {
         Student s = new Student("Michael");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(s);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
     }
 }
